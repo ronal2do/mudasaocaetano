445934f545d804a9fcebf39496580793933ee62c
System Administrator <root@SOTMAC14.local>
2016-07-06 15:21:13 -0300

    email teste 2

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'postmaster@mudasaocaetano.com.br', 'name' => 'Muda São Caetano'],
 
     /*
     |--------------------------------------------------------------------------
